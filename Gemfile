d6973fa8d48d40e97f9ac6779f8ef4f1895edd16
nicholasnadel <nicholasnadel@gmail.com>
2014-11-05 02:02:17 -0800

    struggling with seed data

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
  gem 'bootstrap-sass', '~> 3.1.1'
-
+ gem 'faker'
+ 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create todo lists
+50.times do
+  TodoList.create(
+    todo_item:  Faker::Lorem.sentence,
+    content:   Faker::Lorem.paragraph 
+  )
+end
+tasks = Task.all #unsure about this 
+
+
+puts "Seed finished"
+puts "#{TodoList.count} tasks created"
