27e25c44df96abe896218e8a40282265f448636e
nicholasnadel <nicholasnadel@gmail.com>
2014-11-06 17:58:09 -0800

    updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :todo_lists  #should obviously be has_one as per bloc's expectations
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   has_one :todo_list
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
