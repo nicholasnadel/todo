fd79449928e8f95f75070bb17fc0938be8a48f2c
nicholasnadel <nicholasnadel@gmail.com>
2014-12-04 12:45:26 -0800

    added basic & buggy hashtag parsing

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,14 +4,18 @@ class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create]
 
   def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
+    sender = params['sender']
+    subject = params['subject']
+    body_plain = params["body-plain"]
+    @user = User.find_by_email(sender)
 
-    # You put the message-splitting and business
-    # magic here. 
-
-    # Assuming all went well. 
+    user = User.where(email: params["sender"]).first
+    if !user  
+      head 200 && return
+    end
+    user.bookmarks.create(url: params["body-plain"])
     head 200
+    url = /\b(([\w-]+:\/\/?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:<>"]\s]|\/)))/.match(decoded_body)
+    url[0]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203225024) do
+ActiveRecord::Schema.define(version: 20141204173536) do
+
+  create_table "bookmarks", force: true do |t|
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
