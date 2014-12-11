cee95882c833f99dfc915d84b0d75277ba6d8ef6
nicholasnadel <nicholasnadel@gmail.com>
2014-12-11 17:09:15 +0000

    nitrous

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,33 +1,60 @@
 class BookmarksController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+ 	before_filter :authorize, only: [:edit, :update, :destroy]
+	
+	def index
+		if params[:tag]
+			@bookmarks = Bookmark.tagged_with(params[:tag])
+		else
+			@bookmarks = Bookmark.all 
+		end
+	end
 
-  def index
- 	@bookmarks = Bookmark.all
-  end
+	def show
+		@bookmark = Bookmark.find(params[:id])
+	end
 
-  def new
-  	@bookmark = Bookmark.new
-  end
+	def new
+		@bookmark = Bookmark.new
+	end
 
-  def show
-  	@bookmark = Bookmark.find(params[:id])
-  end
+	def edit
+		@bookmark = Bookmark.find(params[:id])
+	end
 
-  def edit
-  	@bookmark = Bookmark.find(params[:id])
-  end
+	def create
+		@bookmark = Bookmark.new(params[:bookmark])
+		@bookmark.user = current_user
+		if @bookmark.save
+			redirect_to bookmarks_path, notice: "Bookmark was saved successfully"
+		else
+			flash[:error] =  "There was an error creating the bookmark"
+			render new
+		end
+	end
 
-  def destroy
-     @bookmark = Bookmark.find(params[:id])
-     title = @bookmark.url
-    
-     if @bookmark.destroy
-       
-       flash[:notice] = "\"#{title}\" was deleted successfully."
-       redirect_to bookmarks_path
-       else
-       flash[:error] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+	def update
+		@bookmark = Bookmark.find(params[:id])
+		if @bookmark.update_attributes(params[:bookmark])
+			redirect_to bookmarks_path, notice: "Bookmark was updated successfully."
+		else
+			flash[:error] = "There was error updating the Bookmark, not sure what though"
+			render :edit
+		end
+	end
+
+	def destroy
+		@bookmark = Bookmark.find(params[:id])
+		name = @bookmark.title
+
+    if @bookmark.destroy
+      flash[:notice] = "\"#{name}\" bookmark was deleted successfully."
+      redirect_to bookmarks_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :index
+    end
+
+	end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		@user.role = :member
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to root_url, notice: "Thank you for signing up!"
+		else
+			render "new"
+		end
+		@user.update_attribute(:role, 'member')
+	end
+
+	def show
+		@user = params[:id] ? User.find(params[:id]) : current_user
+
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @bookmark.update_attributes(params[:bookmark])
+			redirect_to @bookmark
+		else
+			flash[:error] = "Error saving bookmark. Please try again"
+			render :edit
+		end
+	end
+
 end
